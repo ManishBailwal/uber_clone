c36ad0abac42235bd304472942d1d78546579dca
ManishBailwal <102870593+ManishBailwal@users.noreply.github.com>
2025-09-01 20:57:14 +0530

    completed basic authentication of users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,27 @@ const router = express.Router();
 
 const {body} = require('express-validator')
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 characters long'),
-    body('fullname.password').isLength({min:5}).withMessage('Password must be at least 6 character long'),
+    body('password').isLength({min:5}).withMessage('Password must be at least 6 character long'),
 
 
 ],
 userController.registerUser )
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+     body('password').isLength({min:5}).withMessage('Password must be at least 6 character long'),
+
+],
+userController.loginUser
+)
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile);
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router;
